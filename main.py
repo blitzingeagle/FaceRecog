5560855202e2b299191640c9487b2753e4613bf1
blitzingeagle <mcmorrischen@gmail.com>
2018-05-25 23:44:44 +0800

    If fake_news use CNN because CNN is fake news

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from PIL import Image
 import face_recognition
 
+fake_news = True
 image = face_recognition.load_image_file("images/biden.jpg")
 
-face_locations = face_recognition.face_locations(image)
+if fake_news:
+    face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+else:
+    face_locations = face_recognition.face_locations(image)
 
 print("I found {} face(s) in this photograph.".format(len(face_locations)))
 
 for face_location in face_locations:
-
     # Print the location of each face in this image
     top, right, bottom, left = face_location
     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
